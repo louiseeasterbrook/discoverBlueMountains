4211dc76cc5ed70209c5c1e7d75899c7bd11cc7c
louiseeasterbrook <louise.easterbrook@gmail.com>
2021-09-28 17:21:36 +1000

    remove animation and reposition heading

--- javascript/suburb-js.js
+++ javascript/suburb-js.js
@@ -1,102 +1,62 @@
-const ready = function () {
-  const openingBtn = document.querySelector(".overlay");
+//MEDIA SECTION
+const tabsCont = document.querySelectorAll(".media-tabs");
 
-  //title slide in
-  openingBtn.classList.add("overlay-sub-animation");
+//checks if any of the tabs have been clicked
+tabsCont.forEach((tab) =>
+  tab.addEventListener("click", function (e) {
+    // finds parent - to determine if in section1 or section2
+    parent = this.parentNode.id;
+    //finds the tab-button that has been clicked
+    clicked = e.target.closest(".tab-btn-" + parent);
+    //identifies qhich button has been clicked
+    clickedID = e.target.id;
 
-  //MEDIA SECTION
-  const tabsCont = document.querySelectorAll(".media-tabs");
+    // if no button is clicked - exit
+    if (!clicked) {
+      return;
+    }
 
-  //checks if any of the tabs have been clicked
-  tabsCont.forEach((tab) =>
-    tab.addEventListener("click", function (e) {
-      // finds parent - to determine if in section1 or section2
-      parent = this.parentNode.id;
-      //finds the tab-button that has been clicked
-      clicked = e.target.closest(".tab-btn-" + parent);
-      //identifies qhich button has been clicked
-      clickedID = e.target.id;
+    // give variable names to all components
+    let tab = document.querySelectorAll(".tab-btn-" + parent);
+    let image = document.querySelector("#img-" + parent);
+    let video = document.querySelector("#video-" + parent);
 
-      // if no button is clicked - exit
-      if (!clicked) {
-        return;
-      }
+    // remove active class from all tabs
+    tab.forEach((t) => t.classList.remove("active-tab"));
 
-      // give variable names to all components
-      let tab = document.querySelectorAll(".tab-btn-" + parent);
-      let image = document.querySelector("#img-" + parent);
-      let video = document.querySelector("#video-" + parent);
+    // if the image tab is clicked
+    if (clickedID == "image") {
+      clicked.classList.add("active-tab");
+      image.classList.remove("hide");
+      video.classList.add("hide");
+      //hide video controls
+      video.controls = false;
+      //pause video
+      video.pause();
+      //set video time to 0
+      video.currentTime = 0;
+      // if the video tab is clicked
+    } else {
+      clicked.classList.add("active-tab");
+      video.classList.remove("hide");
+      image.classList.add("hide");
+      //show video controls
+      video.controls = true;
+    }
+  })
+);
 
-      // remove active class from all tabs
-      tab.forEach((t) => t.classList.remove("active-tab"));
+//_____________________________________________________________________________
+// NAVIGATION MENU
+const burger = document.querySelector(".burger");
+const background = document.querySelector(".nav-backdrop");
+const links = document.querySelector(".nav-links");
+const oneLink = document.querySelectorAll("top-link");
 
-      // if the image tab is clicked
-      if (clickedID == "image") {
-        clicked.classList.add("active-tab");
-        image.classList.remove("hide");
-        video.classList.add("hide");
-        //hide video controls
-        video.controls = false;
-        //pause video
-        video.pause();
-        //set video time to 0
-        video.currentTime = 0;
-        // if the video tab is clicked
-      } else {
-        clicked.classList.add("active-tab");
-        video.classList.remove("hide");
-        image.classList.add("hide");
-        //show video controls
-        video.controls = true;
-      }
-    })
-  );
-
-  // _______________________________________________________________
-
-  //ELEMENT
-  const allSections = document.querySelectorAll(".section");
-
-  //FUNCTION
-  const revealSection = function (entries, observer) {
-    const [entry] = entries;
-
-    if (!entry.isIntersecting) return;
-
-    //if section is intersection the 'section-hide' class with be removed and a transition class will be added
-    entry.target.classList.remove("section-hide");
-    entry.target.classList.add("slide-in");
-    observer.unobserve(entry.target);
-  };
-
-  //OBSERVER
-  const sectionObserver = new IntersectionObserver(revealSection, {
-    root: null,
-    threshold: 0.02,
-  });
-
-  //LINK
-  allSections.forEach(function (section) {
-    sectionObserver.observe(section);
-    //adds a class to hide all sections
-    section.classList.add("section-hide");
-  });
-
-  //_____________________________________________________________________________
-  // NAVIGATION MENU
-  const burger = document.querySelector(".burger");
-  const background = document.querySelector(".nav-backdrop");
-  const links = document.querySelector(".nav-links");
-  const oneLink = document.querySelectorAll("top-link");
-
-  burger.addEventListener("click", function () {
-    //toggle between show and hide of naviagtion menu
-    background.classList.toggle("show-nav-fade");
-    links.classList.toggle("show-nav-links");
-  });
-};
-
-//trigger all javascript after HTML has loaded
-window.addEventListener("load", ready);
+burger.addEventListener("click", function () {
+  //toggle between show and hide of naviagtion menu
+  background.classList.toggle("show-nav-fade");
+  links.classList.toggle("show-nav-links");
+});
 
 //_____________________________________________________________
